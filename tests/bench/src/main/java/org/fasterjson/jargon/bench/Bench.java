59b4242d5818a41aed602f44e0917c6385c4d0a4
Jussi Virtanen <jussi.virtanen@fasterjson.org>
2022-01-05 15:46:21 +0200

    jargon-bench: Add 'RandomCharAccessJsonParserBench'

---
+++ tests/bench/src/main/java/org/fasterjson/jargon/bench/Bench.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2022 Jargon authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.fasterjson.jargon.bench;
+
+import java.util.concurrent.TimeUnit;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+
+@Warmup(iterations=5, time=1, timeUnit=TimeUnit.SECONDS)
+@Measurement(iterations=5, time=1, timeUnit=TimeUnit.SECONDS)
+@Fork(value=3, jvmArgsAppend={"-XX:+UseParallelGC", "-Xms1G", "-Xmx1G"})
+@State(Scope.Benchmark)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@BenchmarkMode(Mode.SampleTime)
+public abstract class Bench {
+
+    @Benchmark
+    public void baseline() {
+    }
+
+}
